2c225512fd2c5204fb2014c314ae5557e0179d08
Tejas <teja.bhandvalkar@gmail.com>
2024-02-07 13:52:27 +0530

    Initialized class based components

---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
