280b4ce4a0144c314f7e01aa1c0b84db0420d8e9
Tejas <teja.bhandvalkar@gmail.com>
2024-02-08 20:22:31 +0530

    Added Default Image URL in newsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imgUrl} className="card-img-top" alt="..."/>
+          <img src={!imgUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg": imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
@@ -19,6 +19,3 @@ export class NewsItem extends Component {
 }
 
 export default NewsItem
-
-// Default image if imgUrl is Null:
-// https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-videos%2Fnews-background&psig=AOvVaw3FidrHtcCcvX7AuL0mSL3h&ust=1707486343121000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCODAh7nwm4QDFQAAAAAdAAAAABAE
