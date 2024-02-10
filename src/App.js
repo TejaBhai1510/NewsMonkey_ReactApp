e138937cd32b70a2d28490a78d1c317cebfe542b
Tejas <teja.bhandvalkar@gmail.com>
2024-02-10 13:51:37 +0530

    Added links to fetch news category wise

--- src/App.js
+++ src/App.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import './App.css';
-// API Key: 827985a64a544af4a33a05ef2f78c0c4
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>
-        <News pageSize={8} country="in" category="science" />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={8} country="in" category="general" />}> </Route>
+            <Route exact path="/business" element={<News key="business" pageSize={8} country="in" category="business" />}> </Route>
+            <Route exact path="/general" element={<News key="general" pageSize={8} country="in" category="general" />}> </Route>
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={8} country="in" category="entertainment" />}> </Route>
+            <Route exact path="/health" element={<News key="health" pageSize={8} country="in" category="health" />}> </Route>
+            <Route exact path="/science" element={<News key="science" pageSize={8} country="in" category="science" />}> </Route>
+            <Route exact path="/sports" element={<News key="sports" pageSize={8} country="in" category="sports" />}> </Route>
+            <Route exact path="/technology" element={<News key="technology" pageSize={8} country="in" category="technology" />}> </Route>
+          </Routes>
+        </div>
+      </Router>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export class Navbar extends Component {
     render() {
@@ -6,38 +7,35 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsMonkey</a>
+                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">Home</a>
+                                    <Link className="nav-link" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <Link className="nav-link" to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/business">Business</a>
+                                    <Link className="nav-link" to="/general">General</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/general">General</a>
+                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/entertainment">Entertainment</a>
+                                    <Link className="nav-link" to="/Health">Health</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/Health">Health</a>
+                                    <Link className="nav-link" to="/science">Science</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/science">Science</a>
+                                    <Link className="nav-link" to="/sports">Sports</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/sports">Sports</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/technology">Technology</a>
+                                    <Link className="nav-link" to="/technology">Technology</Link>
                                 </li>
                             </ul>
                         </div>
