7a9380d562ff445585ad2327f6b12ac2cc26891a
Tejas <teja.bhandvalkar@gmail.com>
2024-02-10 16:23:59 +0530

    Added time, author & news source to newsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imgUrl, newsUrl} = this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className='my-3'>
         <div className="card">
-          <img src={!imgUrl?"https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg": imgUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a target="_blank" href={newsUrl} className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
-            </div>
+          <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '85%', zIndex: '1' }}>{source}</span>
+          <img src={!imgUrl ? "https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg" : imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
+            <a target="_blank" href={newsUrl} className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
+          </div>
         </div>
       </div>
     )
